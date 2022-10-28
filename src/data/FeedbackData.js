9f8bbd89e8e8b57d5ac24a7f5890b596469cfcbe
kobeobuah <kobeobuah@gmail.com>
2022-10-28 09:31:17 +0000

    feedback app

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'The quick black fox jumps over the lazy bloodclat dog which causes people to cry but yeah here we go again',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'The quick black fox jumps over the lazy bloodclat dog which causes people to cry but yeah here we go again',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'The quick black fox jumps over the lazy bloodclat dog which causes people to cry but yeah here we go again',
+    }
+]
+
+export default FeedbackData
